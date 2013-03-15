25469b4648a3f4808202908d4f9b5110907107d3
Ubuntu <ubuntu@ip-10-68-14-34.ec2.internal>
2013-03-15 21:06:35 +0000

    2 seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@
 #
 # Examples:
 #
+admin_model     = RailsAdmin::AbstractModel.new(User)
+admin_model.new(:email => 'adastreamer@gmail.com', :password => 'kdeyikme', :password_confirmation => 'kdeyikme', :role => 'superadmin').save
+admin_model.new(:email => 'manager@startupreality.com', :password => 'kdeyikme', :password_confirmation => 'kdeyikme', :role => 'manager').save
 #User.create :
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
