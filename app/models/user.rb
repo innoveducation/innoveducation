87638c0358862a5fb9f50a5b0461b4d128e01c5a
Ubuntu <ubuntu@ip-10-68-14-34.ec2.internal>
2013-03-15 18:32:32 +0000

    nestable added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :role
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :role, :position
   # attr_accessible :title, :body
+
+  
+
+  rails_admin do
+#    include_all_fields
+    weight -1
+    field :email do
+      help { model = self.abstract_model.model_name.underscore; field = self.name; model_lookup = "admin.help.#{model}.#{field}".to_sym; field_lookup = "admin.help.#{field}".to_sym; I18n.t model_lookup, :help => help, :default => [field_lookup, help] }
+    end
+    nestable_list true
+  end
 end
